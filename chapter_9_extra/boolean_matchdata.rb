b515cb1576f7ba851ba2c7c5c4539489e2a50346
Aaron Wilder <element1028@gmail.com>
2017-04-21 11:04:33 -0700

    finished chapter 8

---
+++ chapter_9_extra/boolean_matchdata.rb
@@ -0,0 +1,11 @@
+def has_a_b?(string)
+  if /b/.match(string)
+    puts "We have a match!"
+  else
+    puts "No match"
+  end
+end
+has_a_b?("basketball")
+has_a_b?("football")
+has_a_b?("hockey")
+has_a_b?("golf")
---
+++ chapter_9_extra/passing_block.rb
@@ -0,0 +1,6 @@
+def take_block(&block)
+  block.call
+end
+take_block do
+  puts "Block being called in the method!"
+end
